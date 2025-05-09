d70338ba46ebc318322f83ca1d73ad7857ff56da
shanmathik007 <shanmathik06@gmail.com>
2025-05-10 00:22:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/footer';
 import Home from './pages/home';
