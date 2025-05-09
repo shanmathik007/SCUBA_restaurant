bda26d66c1cfd85c980618527711535830726c50
shanmathik007 <shanmathik06@gmail.com>
2025-05-10 01:00:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/footer';
+import Home from './pages/home';
+import Menu from './pages/menu';
+import Contact from './pages/contact';
+
 function App() {
   return (
-    <div style={{ fontSize: '30px', padding: '100px', textAlign: 'center' }}>
-      🚀 Hello from SCUBA Restaurant!
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      minHeight: '100vh',
+      margin: 0,
+      padding: 0
+    }}>
+      <Navbar />
+      <div style={{ flex: 1 }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+      <Footer />
     </div>
   );
 }
